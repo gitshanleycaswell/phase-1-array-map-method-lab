6925f3d5bde8efd9afd99a3ce4bd3fb6086e5644
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-28 18:30:32 -0400

    array map method lab

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ return tutorials.map(sentence => {
+  const words = sentence.split(" ")
+  const capitalizedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  })
+  return capitalizedWords.join(" ")
+})
 }
+  titleCased()
